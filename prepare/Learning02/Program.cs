1515065edcd765cf51c578fa979d28c4294f2aba
rawinolivera <oli18001@byui.edu>
2024-06-22 12:01:16 -0300

    W02 Prepare: Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Anka Rent A Car";
+        job1._jobTitle = "Adminstrative";
+        job1._startYear = 2016;
+        job1._endYear = 2022;
+        Job job2 = new Job();
+        job2._company = "Bloom";
+        job2._jobTitle = "Peer Mentor";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Rawin Olivera";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
